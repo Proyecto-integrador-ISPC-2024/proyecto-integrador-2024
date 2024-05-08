99b64ff6dfe1249741d37f17a16b6184a9e4081d
0xtian.js <bitrec.61@gmail.com>
2024-05-08 11:47:53 -0300

    Agrego estructuración de la arquitectura del proyecto

---
+++ frontend/src/modules/client-dashboard/client-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-dashboard.component.html',
+  styleUrl: './client-dashboard.component.css'
+})
+export class ClientDashboardComponent {
+
+}
