ef04fe3ade9e1f3a8b173f6565b1092461aa9d50
pabloscarponi <pabloscarponi@gmail.com>
2024-05-09 23:35:15 -0300

    Agrego contenido del componente footer y ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "footer", component:FooterComponent   }
+];
