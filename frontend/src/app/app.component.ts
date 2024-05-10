3fc2458fcbc434d503c3d97099a64ddf0a828129
María Victoria <victoriacursos2022@gmail.com>
2024-05-09 22:04:07 -0300

    primera prueba de components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingCardComponent } from './components/landing-card/landing-card.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet,LandingCardComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Tienda de campeones - Home';
+  title = 'Tienda de Campeones - Home';
 }
---
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+}
