d0574a758ef0a69e6a743e79398f5b9ef9f4d82e
0xtian.js <bitrec.61@gmail.com>
2024-05-12 15:58:52 -0300

    Agrego ventanas modales para formularios de inicio de sesión y de registro | Ajusto enrutamiento en header | Comienzo desarrollo del componente button para cambiar entre tema light y dark

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutUsComponent },
@@ -13,5 +14,9 @@ export const routes: Routes = [
     {
         path: 'cart',
         component: CartComponent,
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
