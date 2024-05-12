6d150f5f540f91259e2348020431abfa9cf0e912
Denisse Araceli Sabeff <denissesabeff@gmail.com>
2024-05-12 11:28:02 -0300

    Agrego validaciones al formulario reactivo de confirmación de compra

--- frontend/src/app/components/cart-resume/cart-resume.component.ts
+++ frontend/src/app/components/cart-resume/cart-resume.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart-resume',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cart-resume.component.html',
   styleUrl: './cart-resume.component.css'
 })
 export class CartResumeComponent {
+  formBuilder = inject(FormBuilder);
+  formGroup = this.formBuilder.nonNullable.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email], []],
+    phone: ['', Validators.required],
+    payment: ['', Validators.required],
+    terms: ['', Validators.requiredTrue]
+  })
+
+  clickRegister(): void {
+    const name = this.formGroup.controls.name.value;
+    const email = this.formGroup.controls.email.value;
+    const phone = this.formGroup.controls.phone.value;
+    const payment = this.formGroup.controls.payment.value;
+    const terms = this.formGroup.controls.terms.value;
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(payment);
+    console.log(terms);
+  }
+
+  onEnviar(event: Event) {
+    console.log(this.formGroup.value)
+    event.preventDefault;
+    if (this.formGroup.valid) {
+      alert("Enviando formulario al servidor...")
+    }
+    else {
+      this.formGroup.markAllAsTouched(),
+        alert("Por favor, complete todos los campos.");
+    }
+  }
+
 
 }
