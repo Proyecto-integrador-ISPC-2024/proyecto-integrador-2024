87906492aaaf3bba704fc8736fceb138e16fd7ef
0xtian.js <bitrec.61@gmail.com>
2024-05-13 17:22:26 -0300

    Agrego rutas para dashboard de cliente y admin | ajusto íconos en header | ajusto layout en /dashboard

--- frontend/src/app/pages/client-dashboard/client-dashboard.component.ts
+++ frontend/src/app/pages/client-dashboard/client-dashboard.component.ts
@@ -1,17 +1,15 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductsSummaryComponent } from '../../components/products-summary/products-summary.component';
 import { OrdersSummaryComponent } from '../../components/orders-summary/orders-summary.component';
 import { ProductsSuggestComponent } from '../../components/products-suggest/products-suggest.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-client-dashboard',
   standalone: true,
-  imports: [CommonModule, ProductsSummaryComponent, OrdersSummaryComponent, ProductsSuggestComponent, RouterLink],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  imports: [ProductsSummaryComponent, OrdersSummaryComponent, ProductsSuggestComponent],
+  templateUrl: './client-dashboard.component.html',
+  styleUrl: './client-dashboard.component.css'
 })
-export class DashboardComponent {
+export class ClientDashboardComponent {
 
 }
