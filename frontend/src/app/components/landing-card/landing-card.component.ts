8058943114adad7b51935380f53b791a2c6e9234
María Victoria <victoriacursos2022@gmail.com>
2024-05-12 22:59:33 -0300

    enlaces botones

--- frontend/src/app/components/landing-card/landing-card.component.ts
+++ frontend/src/app/components/landing-card/landing-card.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProductsComponent } from '../../pages/products/products.component';
 
 @Component({
   selector: 'app-landing-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ProductsComponent],
   templateUrl: './landing-card.component.html',
   styleUrl: './landing-card.component.css'
 })
