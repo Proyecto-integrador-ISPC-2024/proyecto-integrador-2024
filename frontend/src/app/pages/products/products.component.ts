e7050c1fa4e174704fc001f8b8e41d225ff7c34b
Alvarez Jesica <78943636+Js-Alvarez@users.noreply.github.com>
2024-06-03 23:48:57 -0300

    Commit de prueba

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,35 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule, NgForOf, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { CardProductComponent } from '../../components/card-product/card-product.component';
 import { ApiService } from '../../../services/api.service';
 import { Product } from '../../../interfaces/product';
-// import { Products } from '../../../interfaces/products';
 
 @Component({
   selector: 'app-products',
-  standalone: true,
-  imports: [CommonModule, CardProductComponent, NgForOf, NgIf],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
-  private urlProducts = 'https://664d5d12ede9a2b556534efe.mockapi.io/products';
+  private urlTemplate = 'https://664d5d12ede9a2b556534efe.mockapi.io/products';
 
-  // categories: string[] = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'];
-  // selectedCategory: string = this.categories[0];
+  countries: string[] = ['Argentina', 'Brasil', 'Italia']; // Lista de países disponibles
+  selectedCountry: string = this.countries[0];
 
   products: Product[] = [];
-  // filteredProducts: Product[] = [];
+  filteredProducts: Product[] = [];
 
   fetchProducts(): void {
-    this.apiService.get<Product[]>(this.urlProducts).subscribe({
+    this.apiService.get<Product[]>(this.urlTemplate).subscribe({
       next: (data: Product[]) => {
         console.log('Fetched data:', data);
         if (Array.isArray(data)) {
           this.products = data;
-          // this.filteredProducts();
+          this.filterProductsByCountry();
         } else {
           console.error('Data structure is incorrect:', data);
         }
@@ -43,14 +40,16 @@ export class ProductsComponent implements OnInit {
     });
   }
 
-  /* filterProducts(): void {
-    this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
-  } */
+  filterProductsByCountry(): void {
+    // Filtrar los productos cuyo nombre contiene el nombre del país seleccionado
+    this.filteredProducts = this.products.filter(product => product.name.toLowerCase().includes(this.selectedCountry.toLowerCase()));
+  }
 
-  /* onCategoryChange(category: string): void {
-    this.selectedCategory = category;
-    this.filterProducts();
-  } */
+
+  onCountryChange(country: string): void {
+    this.selectedCountry = country;
+    this.filterProductsByCountry();
+  }
 
   ngOnInit(): void {
     this.fetchProducts();
