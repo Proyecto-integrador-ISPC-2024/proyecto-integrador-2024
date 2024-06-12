057db303356b052ab933a15dafc55f6bb6519476
Denisse Araceli Sabeff <denissesabeff@gmail.com>
2024-06-11 22:58:08 -0300

    Arreglando urls y peticiones del login

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../../interfaces/user';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { Token } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
@@ -10,18 +11,28 @@ import { tap } from 'rxjs/operators';
 export class AuthService {
   private url = "http://127.0.0.1:8000"; // URL base de tu backend Django
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   createUser(user: User): Observable<any> {
     return this.http.post(`${this.url}/usuarios/`, user);
   }
 
   login(credentials: { email: string, password: string }): Observable<any> {
-    return this.http.post<any>(`${this.url}/api/token/`, credentials).pipe(
-      tap(response => {
+    return this.http.post<any>(`${this.url}/login/`, credentials).pipe(
+      /* tap(response => {
         if (response && response.access) {
           localStorage.setItem('token', response.access);
         }
+      }) */
+      map(response => {
+        // Manejar la respuesta
+        if (response && response['token']) {
+          // Almacenar los tokens y el id del usuario en localStorage
+          localStorage.setItem('access_token', response['token']);
+          localStorage.setItem('refresh_token', response['refresh_token']);
+          localStorage.setItem('currentUser', JSON.stringify(response['usuario']));
+        }
+        return response;
       })
     );
   }
@@ -31,7 +42,7 @@ export class AuthService {
   }
 
   getToken(): string | null {
-    return localStorage.getItem('token'); // Obtiene el token del almacenamiento local.
+    return localStorage.getItem('access_token'); // Obtiene el token del almacenamiento local.
   }
 
   isLoggedIn(): boolean {
